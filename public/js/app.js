305c237e561980c60d574be9f8959c384126d525
Rehman <Afiza.Rehman@cbre.com>
2019-07-03 08:33:30 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+console.log('Client side javascript file is loaded!')
+
+
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherform.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const location = search.value
+
+    message2.textContent = 'Loading...'
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+                message1.textContent = data.error
+                message2.textContent = ''
+            } else {
+
+                message1.textContent = data.location 
+                message2.textContent = data.forecast 
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+         })
+    })
+})
+
+//console.log(location)
